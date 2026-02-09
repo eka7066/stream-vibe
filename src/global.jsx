7033737be2909908d2ef81e2a1bc0ff84830bb41
Borekat <84665813+eka7066@users.noreply.github.com>
2026-02-09 18:30:21 +0300

    добавление фавиконки и окончательная настройка global.jsx

--- src/global.jsx
+++ src/global.jsx
@@ -1,5 +1,5 @@
 import '@/styles'
-import { Head } from "minista"
+import { Head } from "minista";
 import Header from "@/layouts/Header";
 import Footer from "@/layouts/Footer/Footer";
 import Content from "@/layouts/Content";
@@ -12,14 +12,19 @@ export default function (props) {
 
     return (
         <>
-            <Head htmlAttributes={{ lang: 'en' }}>
+            <Head htmlAttributes={{lang: 'en'}}>
                 <title>Stream Vibe | {title} </title>
+                <script src="/src/main.js" type="module"/>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                <link rel="manifest" href="/site.webmanifest"/>
             </Head>
-            <Header />
+            <Header/>
             <Content>
                 {children}
             </Content>
-            <Footer />
+            <Footer/>
         </>
-    )
+    );
 }
