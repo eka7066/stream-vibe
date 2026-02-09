ebbae839f16678759ce78c06989ed138f84854d9
Borekat <84665813+eka7066@users.noreply.github.com>
2026-02-09 18:00:46 +0300

    основная настройка css

--- minista.config.js
+++ minista.config.js
@@ -76,7 +76,11 @@ export default defineConfig({
             localsConvention: "camelCaseOnly",
         },
         preprocessorOptions: {
-            scss: {},
+            scss: {
+                additionalData: `
+                @use '@/styles/helpers' as *;`,
+                silenceDeprecations: ['legacy-js-api']
+            },
             less: {},
             stylus: {},
         },
