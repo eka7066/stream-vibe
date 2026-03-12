84114d4d1d18583dca87f9b088a79cbcb83cb63f
Borekat <84665813+eka7066@users.noreply.github.com>
2026-03-12 19:48:52 +0300

    добавление меню и иконок в хедер

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,8 +1,31 @@
 import './Header.scss'
 import Logo from "@/components/Logo"
+import classNames from "classnames";
+import Button from "@/components/Button";
 
 
-const Header = () => {
+const Header = (props) => {
+    const {url,} = props
+
+    const menuItems =[
+        {
+           label: 'Home',
+            href: '/'
+        },
+        {
+            label: 'Movies & Shows ',
+            href: '/movies'
+        },
+        {
+            label: 'Support',
+            href: '/support'
+        },
+        {
+            label: 'Subscriptions',
+            href: '/subscriptions'
+        },
+    ]
+
     return (
         <header className="header">
             <div className="header__inner container">
@@ -10,6 +33,35 @@ const Header = () => {
                   className="header__logo"
                   loading="eager"
               />
+             <nav className="header__menu">
+                 <ul className="header__menu-list">
+                     {menuItems.map(({label , href}, index) => (
+                         <li className="header__menu-item" key={index}>
+                             <a className={classNames('header__menu-link', {
+                                'is-active': href === url
+                             })} href={href}>
+                                 {label}
+                             </a>
+                         </li>                         
+                         ))}
+                 </ul>
+             </nav>
+             <div className="header__actions">
+                <Button
+                    className="header__button"
+                    label="Search"
+                    isLabelHidden
+                    mode="transparent"
+                    iconName="search"
+                />
+                <Button
+                    className="header__button"
+                    label="Notifications"
+                    mode="transparent"
+                    isLabelHidden
+                    iconName="notification"
+                />
+             </div>
             </div>
         </header>
     )
